76132dc1d3373ff87bf55983e7939babd355d76e
Roberto Hinojosa <rhinojosaram@gmail.com>
2022-03-01 00:51:58 -0600

    testing the views end oof the tutorial

--- birdie/tests/test_views.py
+++ birdie/tests/test_views.py
@@ -1,10 +1,13 @@
 import pytest
 
 from django.contrib.auth.models import AnonymousUser
+from django.core import mail
+from django.http import Http404
 from django.test import RequestFactory
 from mixer.backend.django import mixer
+from mock import patch
 
-from ..views import HomeView, AdminView, PostUpdateView
+from ..views import HomeView, AdminView, PostUpdateView, PaymentView
 
 pytestmark = pytest.mark.django
 
@@ -36,7 +39,8 @@ class TestAdminView:
 @pytest.mark.django_db
 class TestPostUpdateView:
     def test_get(self):
-        req = RequestFactory().get('/')
+        req = RequestFactory().post('/')
+        req.user = AnonymousUser
         obj = mixer.blend("birdie.Post")
         response = PostUpdateView.as_view()(req, pk=obj.pk)
         assert response.status_code == 200, 'Should be callable by anyone'
@@ -45,7 +49,27 @@ class TestPostUpdateView:
         post = mixer.blend("birdie.Post")
         data = {'body': 'new body test data'}
         req = RequestFactory().post('/', data=data)
+        req.user = AnonymousUser
         response = PostUpdateView.as_view()(req, pk=post.pk)
         assert response.status_code == 302, 'should redirect to success view'
         post.refresh_from_db()
         assert post.body == 'new body test data', 'should update the post'
+
+    def test_security_(self):
+        user = mixer.blend('auth.User', first_name='Martin')
+        post = mixer.blend('birdie.Post')
+        req = RequestFactory().post('/', data={})
+        req.user = user
+        print(f'req: {req}')
+        with pytest.raises(Http404):
+            PostUpdateView.as_view()(req, pk=post.pk)
+
+
+class TestPaymentView:
+    @patch('birdie.views.stripe')
+    def test_payment(self, mock_stripe):
+        mock_stripe.Charge.return_value = {'id': '1234'}
+        req = RequestFactory().post('/', data={'token': '123'})
+        response = PaymentView.as_view()(req)
+        assert response.status_code == 302, 'should redirect to success url'
+        assert len(mail.outbox) == 1, 'should send email'
--- birdie/views.py
+++ birdie/views.py
@@ -1,6 +1,11 @@
+import stripe
 from django.contrib.auth.decorators import login_required
 from django.contrib.messages import success
+from django.core.mail import send_mail
+from django.http import Http404
+from django.shortcuts import redirect
 from django.utils.decorators import method_decorator
+from django.views import View
 from django.views.generic import TemplateView, UpdateView
 
 from . import models, forms
@@ -22,5 +27,20 @@ class AdminView(TemplateView):
 class PostUpdateView(UpdateView):
     model = models.Post
     form_class = forms.PostForm
-    template_name = 'birdie/update.html'
+    # template_name = 'birdie/update.html'
     success_url = '/'
+
+    def post(self, request, *args, **kwargs):
+        print(f"request: {request}")
+        if getattr(request.user, 'first_name', None) == "Martin":
+            raise Http404()
+        return super(PostUpdateView, self).post(request, *args, **kwargs)
+
+
+class PaymentView(View):
+    def post(self, request, *args, **kwargs):
+        charge = stripe.Charge.create(amount=100, currency='sgd', description='', token=request.POST)
+        send_mail('Payment received', 'Charge {} succed'.format(charge['id']),
+                  'server@example.com',
+                  ['admin@domain.com', ], )
+        return redirect('/')
